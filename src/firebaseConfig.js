c06d2d5ed85e061a1e789e2c315049aa38391e23
meowy8 <mhoey8@hotmail.com>
2024-01-26 21:03:35 +0000

    initial commit

---
+++ src/firebaseConfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD-wydtAV2zE5FZIqegTOeI7VhzTG8AGEY",
+  authDomain: "millimetre-3862b.firebaseapp.com",
+  projectId: "millimetre-3862b",
+  storageBucket: "millimetre-3862b.appspot.com",
+  messagingSenderId: "441364412875",
+  appId: "1:441364412875:web:afac7178d8019d11019e4e",
+  measurementId: "G-4KEF7PB742"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
