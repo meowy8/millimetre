a88df1926e62aa261118cba7e5808940d2502ce9
meowy8 <mhoey8@hotmail.com>
2024-02-06 12:58:51 +0000

    changed profile link in nav to username and profileimg

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,40 @@
 import { Link } from "react-router-dom";
 import { UserAuth } from "../context/AuthContext";
 import LogoutButton from "./LogoutButton";
+import { useEffect, useState } from "react";
+import SmallUserIcon from "./SmallUserIcon";
 
 const NavBar = () => {
+  const [username, setUsername] = useState('')
+  const [profileImg, setProfileImg] = useState('')
+
   const { user, userAccount } = UserAuth();
 
+  useEffect(() => {
+    if (userAccount) {
+      setUsername(userAccount.username)
+      setProfileImg(userAccount.profileImg)
+    }
+    console.log(username)
+  }, [userAccount, username])
   //console.log('user account:', userAccount)
   //console.log('user:', user)
 
   return (
-    <nav className="flex justify-between p-4 bg-[#28143e] font-custom">
+    <nav className="flex justify-between items-center p-4 bg-[#28143e]">
       <div>
         <Link to="/">mm</Link>
       </div>
-      <div className="flex gap-4">
+      <div className="flex gap-4 items-center">
+        {user && 
+          <Link to={`/user/${username}`} className="flex items-center gap-2">
+            <div className="flex w-8 h-8 rounded-full overflow-hidden justify-center">
+              <img src={profileImg} alt="" className="flex w-full h-full"/>
+            </div>
+            {username}
+          </Link>
+        }
         <Link to={"/catalogue"}>Catalogue</Link>
-        {user && <Link to={`/user/${userAccount?.username}`}>Profile</Link>}
         {user ? <LogoutButton /> : <Link to="/signin">Sign In</Link>}
       </div>
     </nav>
