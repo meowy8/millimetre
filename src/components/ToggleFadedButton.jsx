ee3c7e390b8296a65a80949d734933ff1f630732
meowy8 <mhoey8@hotmail.com>
2024-02-12 13:26:26 +0000

    allows toggle faded button visibility

--- src/components/ToggleFadedButton.jsx
+++ src/components/ToggleFadedButton.jsx
@@ -1,7 +1,6 @@
 const ToggleFadedButton = ({ togglefaded, handleToggleChange, user }) => {
   return (
     <div>
-      {user && (
         <div className="text-sm">
           {togglefaded ? (
             <button onClick={handleToggleChange}>Unfade Watched</button>
@@ -9,7 +8,6 @@ const ToggleFadedButton = ({ togglefaded, handleToggleChange, user }) => {
             <button onClick={handleToggleChange}>Fade Watched</button>
           )}
         </div>
-      )}
     </div>
   );
 };
